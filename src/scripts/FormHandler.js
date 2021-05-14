7b369393c45bf6a72bfc5551f8b98e847e3505ff
herokunt <6wdcd9q17d86.scruff@simplelogin.co>
2021-05-14 10:06:18 +0200

    added custom font, refactor assets folder and links to assets

--- src/scripts/FormHandler.js
+++ src/scripts/FormHandler.js
@@ -1,14 +1,21 @@
 class FormHandler {
   constructor() {
     this.form = document.querySelector('.contact');
+    this.consentBox = document.getElementById('consent');
+    this.submitBtn = this.form.querySelector('button');
     this.formTogglers = document.querySelectorAll('.toggler');
     this.events();
   }
 
   events() {
-    this.form.addEventListener('submit', e => this.handleSubmit(e));
+    this.form.addEventListener('submit', e => e.preventDefault());
     this.form.addEventListener('click', e => this.handleClick(e));
+    this.consentBox.addEventListener('change', () => this.toggleSubmit());
     this.formTogglers.forEach(el => el.addEventListener('click', () => this.openForm()));
+  }
+
+  toggleSubmit() {
+    this.submitBtn.disabled = !this.consentBox.checked;
   }
 
   handleClick(e) {
@@ -17,6 +24,9 @@ class FormHandler {
     }
     if (e.target.className === 'contact__minimize') {
       return this.hideForm();
+    }
+    if (e.target.className === 'cta font-regular') {
+      return this.handleSubmit(e);
     }
     if (this.form.classList.contains('contact--hidden')) {
       return this.openForm();
@@ -38,10 +48,28 @@ class FormHandler {
   handleSubmit(e) {
     e.preventDefault();
 
-    // Extract input values
-    // Check honeypot
-    // Check input values
-    // Send email (network Request to backend)
+    // newFormData not working for some reason... ?
+    const name = this.form[0];
+    const email = this.form[1];
+    const phone = this.form[2];
+    const subject = this.form[3];
+    const message = this.form[4];
+    const honeypot = this.form[7];
+
+    if (honeypot.checked) return;
+
+    let errors = false;
+    [name, email, phone, subject, message].forEach(input => {
+      if (input.value === '') {
+        errors = true;
+        input.classList.add('contact__input--error');
+        setTimeout(() => {
+          input.classList.remove('contact__input--error');
+        }, 4000);
+      }
+    });
+
+    if (errors) return;
   }
 }
 
